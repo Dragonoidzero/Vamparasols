cd3fab21ea723c84da6633a142651bd926076323
Jayaram Veeraraghavan <jayaram@jvsrivas.com>
2019-07-26 10:41:11 -0400

    Fixed ServerProxy to extend CommonProxy.  Now the pack works in servers too.

--- src/main/java/com/jvsrivas/vamparasol/proxy/ServerProxy.java
+++ src/main/java/com/jvsrivas/vamparasol/proxy/ServerProxy.java
@@ -1,5 +1,20 @@
 package com.jvsrivas.vamparasol.proxy;
 
+import com.jvsrivas.vamparasol.ModItems;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
 @SuppressWarnings("unused")
-public class ServerProxy {
+public class ServerProxy extends CommonProxy {
+    @Override
+    public void preInit(FMLPreInitializationEvent e) {
+        super.preInit(e);
+    }
+
+    @SubscribeEvent
+    @SuppressWarnings("unused")
+    public static void registerModels(ModelRegistryEvent event) {
+        ModItems.initModels();
+    }
 }
